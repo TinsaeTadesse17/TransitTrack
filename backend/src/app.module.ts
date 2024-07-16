5f1b31257361c31287a39592f9609661179620b7
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-07-16 08:58:30 +0300

    authentication logic added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+console.log('Initial JWT_SECRET:', process.env.JWT_SECRET);
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+        console.log('MONGO_URI:', uri);
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {
+    console.log(
+      'AppModule JWT_SECRET:',
+      this.configService.get<string>('JWT_SECRET'),
+    );
+  }
+}
